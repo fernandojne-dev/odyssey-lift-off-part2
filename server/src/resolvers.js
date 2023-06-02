0c3a37929270ca3ea48c5edae9cee51ef53f9861
Fernando Nuñez <31429242+fernandojne20@users.noreply.github.com>
2023-06-02 17:34:52 -0500

    Chapter 6:  Implementing query resolvers (#3)

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -2,7 +2,14 @@ const resolvers = {
   // returns an array of Tracks that will be used to populate
   // the homepage grid of our web client
   Query: {
-    tracksForHome: () => {}
+    tracksForHome: (_, __, { dataSources }) => {
+      return dataSources.trackAPI.getTracksForHome()
+    }
+  },
+  Track: {
+    author: ({ authorId }, _, { dataSources }) => {
+      return dataSources.trackAPI.getAuthor(authorId)
+    }
   }
 }
 
