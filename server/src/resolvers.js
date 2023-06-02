c74a8380153b1dcb9e48ec06a89a799c75e8c839
Fernando Nuñez <fernandojne.20@gmail.com>
2023-06-02 17:15:41 -0500

    chapter 5: The shape of a resolver

---
+++ server/src/resolvers.js
@@ -0,0 +1,9 @@
+const resolvers = {
+  // returns an array of Tracks that will be used to populate
+  // the homepage grid of our web client
+  Query: {
+    tracksForHome: () => {}
+  }
+}
+
+module.exports = resolvers
